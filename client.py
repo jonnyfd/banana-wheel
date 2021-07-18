311d6303f5af76cd1612fc9505e3726191b4051d
jake <hamblen.jw@gmail.com>
2021-07-17 22:45:16 -0400

    changed to accept cli args as username

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
 # Import socket module 
 import socket             
-
+import sys
 # Create a socket object 
 s = socket.socket()         
  
 # connect to the server on local computer 
 s.connect(('0.0.0.0', 6699)) 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
-# once connected, print "welcome to banana wheel"
-name = input("What is your name? \n")
+# sets players name to command line argument
+name = str(sys.argv[1])
+# sends name to server
 s.send(bytes(name, "utf-8"))
 shortly = s.recv(1024)
 print(shortly.decode("utf-8"))
--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 # import socket and select modules 
 import socket
 import select
-
+import sys
 # the number of connections the server will wait for before starting game
 num_players = 2
 
 # list of clients
 sock_list = list()
+
 
 # specifies IPv4 over TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,12 +28,14 @@ while curr_players < num_players:
 		sock_list.append(clientsocket)
 		curr_players += 1
 		print("New client")
-		clientsocket.send(bytes("Welcome to banana wheel.", "utf-8"))
+		# receives player name. sends personalized hello! message. 
+		who = clientsocket.recv(1024)
+		clientsocket.send(bytes("The game will start shortly " + str(who.decode("utf-8")) + ".", "utf-8"))
 	# if not new client, this means that an old client is sending a message. print it.
 	else:
-		who = active_socket.recv(1024)
-		# makes it personal
-		active_socket.send(bytes("The game will start shortly " + str(who.decode("utf-8")) + ".", "utf-8"))
+		 msg = active_socket.recv(1024)
+		 print(msg)
+
 # lets go
 print("All players are ready")
 
